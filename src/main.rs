33309bb91edb49ae83781a95bf7530a26b7a7cc4
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-04 12:31:15 -0500

    basically the python code ported to rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::f32;
+use parser::parse_file;
+use image::Image;
+use color::Color;
+use matrix::Matrix;
+use std::result::Result;
 mod parser;
 mod matrix;
 mod image;
 mod color;
 
 fn main() {
-    println!("Hello, world!");
-    println!("{}", f32::sin(f32::consts::FRAC_PI_2));
+    // println!("Hello, world!");
+    // println!("{}", f32::sin(f32::consts::FRAC_PI_2));
+    let mut screen = Image::new(500, 500);
+    let mut color = Color::new_color(0, 255, 0);
+    let mut edges = Matrix::new(4, 4);
+    let mut transform = Matrix::new(4,4);
+    parse_file("script", edges, transform, screen, color);
 }

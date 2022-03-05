54530785ff32ae130e27b04ea780148cefeba112
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-04 23:48:34 -0500

    fix issue where something gets drawn 0 0

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     // println!("{}", f32::sin(f32::consts::FRAC_PI_2));
     let mut screen = Image::new(500, 500);
     let mut color = Color::new_color(0, 255, 0);
-    let mut edges = Matrix::new(4, 4);
+    let mut edges = Matrix::new(0,0);
     let mut transform = Matrix::new(4,4);
     parse_file("script", edges, transform, screen, color);
 }

ce2923e8f37561712b719e0b8457bc8d1847b46c
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-06 10:45:02 -0500

    split option for either art or image

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use parser::parse_file;
 use image::Image;
 use color::Color;
 use matrix::Matrix;
+use std::env;
 mod parser;
 mod matrix;
 mod image;
@@ -9,16 +10,18 @@ mod color;
 mod draw;
 
 fn main() {
-    // println!("Hello, world!");
-    // println!("{}", f32::sin(f32::consts::FRAC_PI_2));
-    let mut screen = Image::new(500, 500);
-    let mut color = Color::new_color(0, 255, 0);
-    let mut edges = Matrix::new(0,0);
-    let mut transform = Matrix::new(4,4);
-    parse_file("script", edges, transform, screen, color);
-    screen = Image::new(500, 500);
-    color = Color::new_color(0, 255, 0);
-    edges = Matrix::new(0,0);
-    transform = Matrix::new(4,4);
-    parse_file("oofscript", edges, transform, screen, color);
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && args[1] == "art"{
+        let screen = Image::new(500, 500);
+        let color = Color::new_color(0, 255, 0);
+        let edges = Matrix::new(0,0);
+        let transform = Matrix::new(4,4);
+        parse_file("oofscript", edges, transform, screen, color);
+    }else{
+        let mut screen = Image::new(500, 500);
+        let mut color = Color::new_color(0, 255, 0);
+        let mut edges = Matrix::new(0,0);
+        let mut transform = Matrix::new(4,4);
+        parse_file("script", edges, transform, screen, color);
+    }
 }
